7d5ad09f09b3fc48c41270d0b2ccc46425675b86
Orrelav <82299511+Re-Brain@users.noreply.github.com>
2025-01-25 14:40:49 +0800

    Initial

---
+++ app/(tabs)/user.tsx
@@ -0,0 +1,22 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const User = (props: Props) => {
+  return (
+    <View style={styles.container}>
+      <Text>Hello</Text>
+    </View>
+  )
+}
+
+export default User
+
+const styles = StyleSheet.create({
+  container : {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  }
+})
--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,32 +1,20 @@
-import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-
-export default function NotFoundScreen() {
+const pageIsNotFound = () => {
   return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+    <View style={styles.container}>
+      <Text>Page is not found</Text>
+    </View>
+  )
 }
 
+export default pageIsNotFound
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
+    container : {
+        flex : 1,
+        alignItems : "center",
+        justifyContent : "center"
+    }
+})
